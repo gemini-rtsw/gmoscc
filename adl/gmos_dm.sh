851bd5b353ffe25792517f442a1af347ad58c558
Pedro <pedro@example.com>
2007-02-09 01:31:16 +0000

    Fixed/added path names

--- adl/gmos_dm.sh
+++ adl/gmos_dm.sh
@@ -1,4 +1,4 @@
 #!/bin/csh -f
-setenv EPICS_DISPLAY_PATH .:/gemini/gmosdc/gmosdc/data
-setenv PATH ${PATH}:/home/ajf/gemini/gmos/bin/solaris
+setenv PATH ${PATH}:/gemini/GEM8.6/gmos/gmos/bin/solaris
+setenv EPICS_DISPLAY_PATH .:/gemini/epics3.13.4/gmosdc/gmosdc/data
 dm2-4 gmosTop.dl "T=gm:, S=gm:, C1=cc:, C2=cc:, CC=cc:, DC=dc:" &
